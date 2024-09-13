34a7f4fd9b7f83b621bf62d3610a24afd0abfb76
Nikita <nikitaeug@gmail.com>
2024-09-13 10:35:31 +0200

    commit fixes

--- routes/web.php
+++ routes/web.php
@@ -5,7 +5,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('homepage');
-});
+})->name('homepage');
 
 Route::get('/dashboard', function () {
     return view('dashboard');
