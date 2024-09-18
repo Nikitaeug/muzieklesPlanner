3225605d6fcc1b4d4a139044df53d1bcc0294882
Nikita <nikitaeug@gmail.com>
2024-09-18 13:34:05 +0200

    removed settings from nav

--- resources/views/layouts/ingelogdnav.blade.php
+++ resources/views/layouts/ingelogdnav.blade.php
@@ -24,11 +24,6 @@
                     <span class="ml-2">Profile</span>
                 </a>
             </li>
-            <li class="mb-4">
-                <a href="/profile/settings" class="flex items-center p-2 text-white rounded transition duration-300 hover:bg-blue-600">
-                    <i class="text-xl iconoir-settings"></i>
-                    <span class="ml-2">Settings</span>
-                </a>
             </li>
             <li>
                 <form method="POST" action="{{ route('logout') }}">
--- routes/web.php
+++ routes/web.php
@@ -28,9 +28,7 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-    
-    Route::get('/profile/settings', [ProfileController::class, 'settings'])->name('profile.settings');
-});
+    });
 
 // Agenda routes
 Route::middleware('auth')->group(function () {
