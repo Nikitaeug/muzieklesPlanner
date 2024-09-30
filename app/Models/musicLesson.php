ccce6431410721e3dd6501aa3e8545cd2bf7944c
Etienne023 <evanbergeijk637@student.novacollege.nl>
2024-09-30 13:45:14 +0200

    "hoofdletters models"

--- app/Models/musicLesson.php
+++ app/Models/musicLesson.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class MusicLesson extends Model
+class musicLesson extends Model
 {
     use HasFactory;
 
